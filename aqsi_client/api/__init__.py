8bad7140dac89a37f4e638d977c8bc0e5150ca63
eugenkonyshev <mktt94@gmail.com>
2025-05-17 19:53:57 +0300

    правки структуры проекта

--- aqsi_client/api/__init__.py
+++ aqsi_client/api/__init__.py
@@ -1,8 +1,10 @@
 """A client library for accessing aQsi API V3"""
 
 from client import AuthenticatedClient, Client
+from api.v3.api.goods import Goods
 
 __all__ = (
     "AuthenticatedClient",
     "Client",
+    "Goods",
 )
